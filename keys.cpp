e7c7cf88fbdda25bc9b1a344ef1e2d136c5b0249
Aleksandar Kodzhabashyiski <sashyk6343o@gmail.com>
2023-03-29 01:24:32 +0300

    Keys Sofia Open Spring 2019 #2

--- keys.cpp
+++ keys.cpp
@@ -6,6 +6,7 @@ const int mod = 1e9 + 7;
 struct Node
 {
     int wordsEnded = 0;
+    int wordsPassing = 0;
     Node *nextletters[26] = {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
 };
 
@@ -21,43 +22,26 @@ int main()
     std::getline(std::cin, t, '$');
     for (int a = 0; a < t.length(); a++)
     {
-        // std::cout<<t[a];
-
         Node *op = root;
 
         for (; a < t.length() && t[a] != ' ' && t[a] != '\n'; a++)
         {
-            if (t[a + 1] == ' ' or t[a + 1] == '\n' or t[a + 1] == '\0')
-            {
-                //  std::cout<<t[a]<<" "<<op->wordsEnded<<" ";
-                op->wordsEnded++;
-            }
-            else
-            {
-                sum += op->wordsEnded;
-                sum = sum % mod;
-            }
-            // std::cout<<t[a]<<sum<<" ";
-            bool l = true;
+            op->wordsPassing++;
+            sum += op->wordsEnded;
+            sum = sum % mod;
             if (op->nextletters[t[a] - 'a'] != nullptr)
             {
-
-                //  std::cout<<"k ";
-                if (t[a + 1] == ' ' or t[a + 1] == '\n' or t[a + 1] == '\0')
-                {
-                    sum += op->nextletters[t[a] - 'a']->wordsEnded;
-                }
                 op = op->nextletters[t[a] - 'a'];
             }
             else
-            {
-                //  std::cout<<"t ";
+            {      
                 op->nextletters[t[a] - 'a'] = new Node;
                 op = op->nextletters[t[a] - 'a'];
             }
-
-            // std::cout<<sum<<"\n";
         }
+        sum += op->wordsPassing + op->wordsEnded;
+        op->wordsEnded++;
+        sum = sum % mod;
     }
 
     std::cout << sum;
